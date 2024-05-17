bbc86e3a85e0bc1ddd8f1be9e75316bbef415a70
weyne <weyneace@gmail.com>
2024-05-17 11:47:15 -0300

    ok

--- script.js
+++ script.js
@@ -21,9 +21,10 @@ async function searchMovies() {
     const data = await response.json()
     if(data.results.length > 0) {
       const movie = data.results[0]
+      console.log(data)
       showMovie(movie)
     } else {
-      document.getElementById('result').innerHTML = `Não Encontramos nenhum titulo com o nome ${title}`
+      document.getElementById('result').innerHTML = `<h2>Não Encontramos nenhum titulo com o nome <strong>${title.value}</strong></h2>`
     }
   } catch(error) {
     console.error(`Erro ao buscar o filme: ${error}`)
@@ -33,6 +34,31 @@ async function searchMovies() {
 }
 
 function showMovie(movie) {
-  const {original_title, } = movie
-  console.log(movie)
+  const {original_title, poster_path,title, overview, vote_average, release_date, genre_ids} = movie
+  const description = document.getElementById('description')
+  description.innerHTML = `
+      <h1>${title}</h1>
+
+      <h3>Sinopse</h3>
+
+      <p>${overview}</p>
+
+      <ul>
+        <li><span>Nota: </span> <p>${vote_average}</p></li>
+        <li><span>Lançado: </span> <p>${release_date}</p></li>
+        <li><span>Genero: </span> <p>${genre_ids[0]}</p></li>
+        <li><span>titulo original: </span> <p>${original_title}</p></li>
+      </ul>
+
+  `
+  const resultCard = document.querySelector('.card')
+  resultCard.innerHTML = `
+        <img src="https://image.tmdb.org/t/p/w500${poster_path}" alt="">
+        <button><i class='bx bx-movie' ></i> <span>Trailer</span></button>
+        <div class="actions">
+          <a href="#"><i class='bx bxs-star clicked'></i></i><span>Favorito</span></a>
+          <a href="#"><i class='bx bxs-bookmark-star' ></i><span>Assistir mais Tarde</span></a>
+          <a href="#"><i class='bx bx-show'></i><span>Assistido !</span></a>
+        </div>
+  `
 }
